13ac48968e46c5cb8fab2d8bc31c306bf2b8f471
Nikhil Kumar <153814768+Nikhil2247@users.noreply.github.com>
2025-06-05 13:17:06 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser'; 
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +8,8 @@ async function bootstrap() {
     origin: '*', // Adjust this to your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-   app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as needed
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); 
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as needed
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
